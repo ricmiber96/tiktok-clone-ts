b7a2b12a454a7b0103e6cbf53c641eaff761245e
Ricardo Miralles <ricmiber@gmail.com>
2022-11-14 17:23:46 +0100

    Initial commit 🚀

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <main>
+        <p>Tik Tok clone</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
